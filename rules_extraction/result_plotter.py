25ee71e18a2a0ba4ea33043114b788a50de5ab3b
arthurbabey <a.babey5@gmail.com>
2024-01-10 16:03:31 +0100

    change logic from class to function

--- rules_extraction/result_plotter.py
+++ rules_extraction/result_plotter.py
@@ -3,78 +3,72 @@ import numpy as np
 import torch
 import torchvision.transforms as transforms
 from PIL import Image
+from rule_handler import RuleHandler
 
-from .rule_handler import RuleHandler
+
+def _accuracy_N_rules(rules, X_test, y_test, N):
+    """
+    Compute accuracy using N rules.
+
+    :param X_test: Test data features.
+    :param y_test: Test data labels.
+    :param N: Maximum number of rules to consider.
+    :return: Tuple (n_rules_used, scores)
+    """
+    scores = []
+    n_rules_used = list(range(1, N + 1, 2))
+    for n in n_rules_used:
+        rule_handler = RuleHandler(rules=rules[:n])  # Limiting to n rules
+        score = rule_handler.score(X_test, y_test, rule_handler.rules)
+        scores.append(score)
+
+    return n_rules_used, scores
 
 
-class ResultPlotter:
-    def __init__(self, rules):
-        self.rules = rules
+def plot_accuracy(rules, X_test, y_test, class_name=None, N=5, save_path=None):
+    """
+    Plots and optionally saves a plot of accuracy vs. number of rules.
 
-    def _accuracy_N_rules(self, X_test, y_test, N):
-        """
-        Compute accuracy using N rules.
+    :param X_test: test data features
+    :param y_test: test data labels
+    :param class_name: string, name of the class
+    :param N: int, maximum number of rules to consider
+    :param save_path: str, if provided, the path where the plot will be saved
+    """
+    n_rules_used, scores = _accuracy_N_rules(rules, X_test, y_test, N)
 
-        :param X_test: Test data features.
-        :param y_test: Test data labels.
-        :param N: Maximum number of rules to consider.
-        :return: Tuple (n_rules_used, scores)
-        """
-        scores = []
-        n_rules_used = list(range(1, N + 1, 2))
-        for n in n_rules_used:
-            rule_handler = RuleHandler(rules=self.rules[:n])  # Limiting to n rules
-            score = rule_handler.score(X_test, y_test, rule_handler.rules)
-            scores.append(score)
+    # Plotting logic starts here
+    plt.figure(figsize=(10, 6))
+    plt.plot(n_rules_used, scores, marker="o", linestyle="-", color="b", linewidth=1)
 
-        return n_rules_used, scores
+    # Adding titles and labels
+    plt.title("Accuracy vs. Number of Rules Used")
+    plt.xlabel("Number of Top Rules Selected")
+    plt.ylabel("Accuracy")
 
-    def plot_accuracy(self, X_test, y_test, class_name=None, N=5, save_path=None):
-        """
-        Plots and optionally saves a plot of accuracy vs. number of rules.
+    # Adjusting the x-axis labels
+    plt.xticks(n_rules_used, [f"Top {n}" for n in n_rules_used])
 
-        :param X_test: test data features
-        :param y_test: test data labels
-        :param class_name: string, name of the class
-        :param N: int, maximum number of rules to consider
-        :param save_path: str, if provided, the path where the plot will be saved
-        """
-        n_rules_used, scores = self._accuracy_N_rules(X_test, y_test, N)
+    # Optionally adding class name to the plot
+    if class_name:
+        plt.legend([f"Class: {class_name}"])
 
-        # Plotting logic starts here
-        plt.figure(figsize=(10, 6))
-        plt.plot(
-            n_rules_used, scores, marker="o", linestyle="-", color="b", linewidth=1
-        )
+    # Adding a grid with darker gray lines
+    plt.grid(True, linestyle="--", alpha=0.7, color="#a0a0a0")
 
-        # Adding titles and labels
-        plt.title("Accuracy vs. Number of Rules Used")
-        plt.xlabel("Number of Top Rules Selected")
-        plt.ylabel("Accuracy")
+    # Setting background color to very light grey
+    plt.gca().set_facecolor("#f0f0f0")
 
-        # Adjusting the x-axis labels
-        plt.xticks(n_rules_used, [f"Top {n}" for n in n_rules_used])
+    # Save the plot if a save path is provided
+    if save_path:
+        try:
+            plt.savefig(save_path)
+            print(f"Plot saved at: {save_path}")
+        except Exception as e:
+            raise RuntimeError(f"Couldn't save the plot due to: {str(e)}") from e
 
-        # Optionally adding class name to the plot
-        if class_name:
-            plt.legend([f"Class: {class_name}"])
-
-        # Adding a grid with darker gray lines
-        plt.grid(True, linestyle="--", alpha=0.7, color="#a0a0a0")
-
-        # Setting background color to very light grey
-        plt.gca().set_facecolor("#f0f0f0")
-
-        # Save the plot if a save path is provided
-        if save_path:
-            try:
-                plt.savefig(save_path)
-                print(f"Plot saved at: {save_path}")
-            except Exception as e:
-                raise RuntimeError(f"Couldn't save the plot due to: {str(e)}") from e
-
-        # Displaying the plot
-        plt.show()
+    # Displaying the plot
+    plt.show()
 
     def plot_rule_frontier(
         self, df, rule, target_class, model=None, alpha=0.65, save_path=None
